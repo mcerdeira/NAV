974fec810f5910045da2773711736844dc54c378
Martín Cerdeira <martincerdeira@gmail.com>
2017-04-28 16:06:58 -0300

    Initial commit
    Extract source for commiting to TFS, etc

---
+++ PS Scripts/Extract/Export Base.ps1
@@ -0,0 +1,14 @@
+﻿Set-ExecutionPolicy unrestricted
+
+Import-Module "C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1" | Out-Null
+
+$NavIde = "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\finsql.exe"
+$NavModelTools = "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\NavModelTools.ps1"
+. $NavModelTools $NavIde | Out-Null
+
+$DemoDatabase = "NAV2017ORIGBASE"
+$SqlServer = "ARNAV001"
+$BaseFolder = "C:\NAV\PS scripts\Export Source\"
+$SourceFileName = (Join-Path $BaseFolder "Base.txt")
+
+Export-NAVApplicationObject -Verbose -DatabaseName $DemoDatabase -DatabaseServer $SqlServer -Path $SourceFileName
---
+++ PS Scripts/Extract/Extract.ps1
@@ -0,0 +1,24 @@
+﻿Set-ExecutionPolicy unrestricted
+
+Import-Module "C:\Program Files\Microsoft Dynamics NAV\100\Service\NavAdminTool.ps1" | Out-Null
+
+$NavIde = "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\finsql.exe"
+$NavModelTools = "C:\Program Files (x86)\Microsoft Dynamics NAV\100\RoleTailored Client\NavModelTools.ps1"
+. $NavModelTools $NavIde | Out-Null
+
+$DemoDatabase = "NAV2017DEVBASE"
+$SqlServer = "ARNAV001"
+$BaseFolder = "C:\NAV\PS scripts\Export Source\"
+$ModifiedFileNAme = (Join-Path $BaseFolder "Modified.txt")
+$SourceFileName = (Join-Path $BaseFolder "Base.txt")
+$DeltaFolder = (Join-Path $BaseFolder "Delta")
+
+Export-NAVApplicationObject -Verbose -DatabaseName $DemoDatabase -DatabaseServer $SqlServer -Path $ModifiedFileNAme
+
+if(Test-Path $DeltaFolder)
+{
+    Remove-Item -Path $DeltaFolder -Recurse -Force
+}
+mkdir $DeltaFolder
+
+Compare-NAVApplicationObject -Verbose -Original $SourceFileName -Modified $ModifiedFileNAme -Delta $DeltaFolder
